fab0d8b93fb578f46c998f5931f407fb3c82b6f2
Tabor Wondimu <csmajortabor@gmail.com>
2022-03-04 16:31:01 -0800

    Finished Excercise

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace PracticeLinq
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var gameNames = new List<string>()  {"TheBiggestGame", "CODA" ,"GTAEE", "Mortal Combat", "TheSecondBiggestGame"};
+            var collection = gameNames.OrderBy(n => n.Length).ToList();
+            foreach (var name in collection)
+            {
+                Console.WriteLine(name);    
+            }
+
+        }
+    }
+}
